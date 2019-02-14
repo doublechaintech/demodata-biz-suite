f123bfb4111959a398d05adaf61c4013cf021d6c
philipgreat <philip_chang@163.com>
2019-02-14 19:13:45 +0800

    Update to new version

--- bizcore/WEB-INF/demodata_custom_src/com/test/demodata/image/ImageCustomManager.java
+++ bizcore/WEB-INF/demodata_custom_src/com/test/demodata/image/ImageCustomManager.java
@@ -1,6 +1,8 @@
 package com.test.demodata.image;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Shape;
@@ -67,25 +69,42 @@ public class ImageCustomManager extends ImageManagerImpl {
 		Graphics2D g2 = bufferredImage.createGraphics();
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-		Font f = new Font("Monospaced", Font.BOLD, 14);
-		g2.setFont(f);
-		String text=String.format("%s(%dX%d)", note,internalWidth,internalHeight);
-		g2.drawString(text,1,15); 
+		
+		GradientPaint graytowhite = new GradientPaint(0,0,Color.GRAY,width, 0,Color.WHITE);
+		g2.setPaint(graytowhite);
+		
+		
+		
 		//WritableRaster raster = bufferedImage .getRaster();
 		//DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
 		Shape shape = new Rectangle2D.Double(1, 1,
 				internalWidth-2,
 				internalHeight-2);
-		g2.draw(shape);
+		
+		g2.fill(shape);
 		shape = new Line2D.Double(0, 0, internalWidth, internalHeight);
 		g2.draw(shape);
 		shape = new Line2D.Double(internalWidth, 0, 0, internalHeight);
 		g2.draw(shape);
+		
+		Font f = new Font("Monospaced", Font.BOLD, 14);
+		g2.setFont(f);
+		String text=String.format("%s(%dX%d)", note,internalWidth,internalHeight);
+		g2.drawString(text,1,15); 
+		
+		
+		
+		
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		
 		ImageIO.write(bufferredImage, "png", bos);
 		blob.setData(bos.toByteArray());
 		blob.setMimeType(BlobObject.TYPE_PNG);
+		
+		
+		
+		
+		
 		//BlobObject.TYPE_XLSX
 		return blob;
 		
