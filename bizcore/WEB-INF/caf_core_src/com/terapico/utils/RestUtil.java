dcd5b94603e59a32c4402846931a29167e19bc73
philipgreat <philip_chang@163.com>
2019-02-15 12:15:05 +0800

    Fix output stream

--- bizcore/WEB-INF/caf_core_src/com/terapico/utils/RestUtil.java
+++ bizcore/WEB-INF/caf_core_src/com/terapico/utils/RestUtil.java
@@ -1,6 +1,8 @@
 package com.terapico.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
@@ -28,6 +31,28 @@ public class RestUtil {
 		HttpEntity entity = response.getEntity();
 		String content = EntityUtils.toString(entity, StandardCharsets.UTF_8);
 		return content;
+	}
+
+	public static Object remoteGetObject(String sessionId, String url, Class<?> clazz)
+			throws ClientProtocolException, IOException {
+
+		CloseableHttpClient httpClient = getHttpClient();
+		HttpGet getRequest = new HttpGet("http://localhost:8080/RESTfulExample/json/product/get");
+		getRequest.addHeader("Accept", "application/json");
+		getRequest.addHeader("Cookie", sessionId);
+
+		HttpResponse response = httpClient.execute(getRequest);
+		
+		if (response.getStatusLine().getStatusCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+		ObjectMapper mapper = new ObjectMapper();
+		Object responseObj = mapper.readValue(br, clazz);
+		
+		return responseObj;
+		
 	}
 
 	private static CloseableHttpClient getHttpClient() {
@@ -70,5 +95,5 @@ public class RestUtil {
 		URI uri = builder.build();
 		return uri;
 	}
-	
+
 }
